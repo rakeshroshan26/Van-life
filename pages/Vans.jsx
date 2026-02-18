4df4803bd23308d0dd2d482dcbc58059754dc05d
Rakesh Roshan Parvatapuram <rakeshroshan26p@gmail.com>
2026-02-18 21:38:17 +0530

    Creating route params for Van ID's

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,38 +1,32 @@
 import React from "react"
-
-/**
- * Challenge: Fetch and map over the data to display it on
- * the vans page. For an extra challenge, spend time styling
- * it to look like the Figma design.
- * 
- * Hints:
- * 1. Use `fetch("/api/vans")` to kick off the request to get the
- *    data from our fake Mirage JS server
- * 2. What React hook would you use to fetch data as soon as the
- *    Vans page loads, and only fetch it the one time?
- */
-
+import {Link} from "react-router-dom"
 
 export default function Vans() {
-    const [vans,setVans]=React.useState([])
-    React.useEffect(()=>{fetch("/api/vans")
-    .then(r=>r.json())
-    .then((data)=>{
-   setVans(data.vans)
-    })},[])
-    const gg=vans.map(van=>{
-        return(
-            <>
-           <img src={`${van.imageUrl}`}/>
-           <h2>{van.name}</h2>
-           <p>{van.description}</p>
-           </>
-        )
-    })
+    const [vans, setVans] = React.useState([])
+    React.useEffect(() => {
+        fetch("/api/vans")
+            .then(res => res.json())
+            .then(data => setVans(data.vans))
+    }, [])
+
+    const vanElements = vans.map(van => (
+        <Link to={`/vans/${van.id}`}> <div key={van.id} className="van-tile">
+            <img src={van.imageUrl} />
+            <div className="van-info">
+                <h3>{van.name}</h3>
+                <p>${van.price}<span>/day</span></p>
+            </div>
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        </div>
+        </Link>
+    ))
+
     return (
-        <>
-        <h1>Explore our Van options</h1>
-        {gg}
-        </>
+        <div className="van-list-container">
+            <h1>Explore our van options</h1>
+            <div className="van-list">
+                {vanElements}
+            </div>
+        </div>
     )
 }
