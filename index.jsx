f074317a49f1bd3b4d84025be25b7c883bafb434
Rakesh Roshan Parvatapuram <rakeshroshan26p@gmail.com>
2026-02-18 21:36:45 +0530

    CSS styling and fetching
    Fetching and mapping over the data to displaying it on the vans page

--- index.jsx
+++ index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
+
 import "./server"
 
 function App() {
@@ -19,7 +20,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />}/>
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   )
